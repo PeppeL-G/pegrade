e56e06effa239a449e6ffe0649a1515238a23059
Peppe L-G <peter.larsson-green@gmail.com>
2022-09-08 18:25:43 +0200

    Implemented the movies page. Closes #2.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const data = require('./data.js')
 
 const app = express()
 
@@ -13,6 +14,16 @@ app.use(
 
 app.get('/', function(request, response){
 	response.render('start.hbs')
+})
+
+app.get('/movies', function(request, response){
+	
+	const model = {
+		movies: data.movies
+	}
+	
+	response.render('movies.hbs', model)
+	
 })
 
 app.listen(8080)
---
+++ data.js
@@ -0,0 +1,13 @@
+exports.movies = [{
+	id: 1,
+	title: "GoldenEye",
+	grade: 9
+}, {
+	id: 2,
+	title: "Shrek",
+	grade: 8
+}, {
+	id: 3,
+	title: "Dumb & Dumber",
+	grade: 9
+}]
